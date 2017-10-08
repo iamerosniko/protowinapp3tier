c9d9d644130960d2062316f671ae86e44cf64f72
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-10-08 18:28:39 +0800

    up

---
+++ DAL/TeamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class TeamController
+    {
+        private PWContext db = new PWContext();
+        public List<PW_Teams> get()
+        {
+            return db.PW_Teams.ToList();
+        }
+        public PW_Teams get(System.Guid id)
+        {
+            var team = db.PW_Teams.Find(id);
+            return team;
+        }
+    }
+}
---
+++ protowinapp3tier/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAL;
+namespace protowinapp3tier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TeamController tc = new TeamController();
+            List<PW_Teams> teams = tc.get();
+            PW_Teams team = tc.get(new Guid("665bef80-9692-4550-80bb-01ff91e5829e"));
+        }
+
+
+    }
+}
